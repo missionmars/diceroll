20be0516aa3da7b30b71eb786074593576d566ad
Tyrone <tyrone@designroom.co.za>
2018-01-01 18:58:01 +0200

    Few updates

--- dice.py
+++ dice.py
@@ -2,6 +2,9 @@ import random
 from time import sleep 
 
 def lets_roll():
+	print "1 - 10 How hard do you want to roll?"
+	roll_speed = raw_input()
+
 	dice_roll = random.randint(1,int(roll_speed))
 
 	for x in range(dice_roll):
@@ -9,9 +12,6 @@ def lets_roll():
 		sleep(1)
 		if x == dice_roll-1:
 			print 'Dice landed on %s' % (random.randint(1,6))
-
-print "1 - 10 How hrd do you want to roll?:"
-roll_speed = raw_input()
 
 lets_roll()
 
