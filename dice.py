130378b53fe6bcd3612240f70b31e30e4a1d4ff7
Tyrone <tyrone@designroom.co.za>
2018-01-01 18:24:37 +0200

    Updating gen

--- dice.py
+++ dice.py
@@ -1,8 +1,24 @@
 import random
+from time import sleep 
 
-dice_roll = random.randint(3,30)
+def lets_roll():
+	dice_roll = random.randint(1,int(roll_speed))
 
-for x in range(dice_roll):
-	print 'Dice rolling...'
-	if x == dice_roll-1:
-		print 'Dice landed on %s' % (random.randint(1,6))
+	for x in range(dice_roll):
+		print 'Dice rolling...'
+		sleep(1)
+		if x == dice_roll-1:
+			print 'Dice landed on %s' % (random.randint(1,6))
+
+print "1 - 10 How hrd do you want to roll?:"
+roll_speed = raw_input()
+
+lets_roll()
+
+print "Roll again? (Y/N)"
+roll_again = raw_input()
+
+if roll_again == 'Y':
+	lets_roll()
+else:
+	exit()
